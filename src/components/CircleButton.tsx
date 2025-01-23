b5b8cd967efef97c28d6f25d0d13494bcbdaa94b
yuzukiti <yuzukiti2027@gmail.com>
2025-01-23 13:43:58 +0900

    components create

---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+interface Props {
+  children: string;
+}
+
+const CircleButton = (props: Props): JSX.Element => {
+  const { children } = props;
+  return (
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#33b251',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 48,
+  },
+});
+
+export default CircleButton;
